1ef2435376462b3512cfec941bd9ce5ac8efcceb
jmngandu <jamesmngandu@gmail.com>
2024-11-05 09:22:49 +0300

    pro

--- pro/src/appwrite/config.js
+++ pro/src/appwrite/config.js
@@ -1,7 +1,7 @@
 import { Client, Databases } from "appwrite";
 const client = new Client();
 client.setEndpoint(import.meta.env.VITE_ENDPOINT);
-client.setProject("67288cd7001e2e5ac80a");
+client.setProject(import.meta.env.VITE_PROJECT_ID);
 
 const database = new Databases(client);
 
