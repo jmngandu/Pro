c72bf6193bc1694b0b0fcf93cd45e473fe6fce6d
jmngandu <jamesmngandu@gmail.com>
2024-11-05 09:33:35 +0300

    pro

--- pro/src/pages/Notes.jsx
+++ pro/src/pages/Notes.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import { database } from "../appwrite/config";
 function Notes() {
+  const [notes, setNotes] = useState([]);
+
   const init = async () => {
     const response = await databases.listDocuments(
       import.meta.env.DATABASE_ID,
       import.meta.env.VITE_COLLECTION_ID_NOTES
     );
-    console.log(response);
+    setNotes(response.documents);
   };
   return <div>Notes</div>;
 }
