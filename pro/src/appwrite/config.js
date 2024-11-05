6a1917181afe9357e9308dee9d0649972dc46af3
jmngandu <jamesmngandu@gmail.com>
2024-11-05 09:22:24 +0300

    pro

--- pro/src/appwrite/config.js
+++ pro/src/appwrite/config.js
@@ -1,6 +1,6 @@
 import { Client, Databases } from "appwrite";
 const client = new Client();
-client.setEndpoint("https://cloud.appwrite.io/v1");
+client.setEndpoint(import.meta.env.VITE_ENDPOINT);
 client.setProject("67288cd7001e2e5ac80a");
 
 const database = new Databases(client);
